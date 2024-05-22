cf2b0970442055320923947404d38e3d25d12583
holail14 <kev20duvin8@gmail.com>
2024-05-22 15:28:52 +0200

    Add list component with creation form

---
+++ actions/create-list/schema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const CreateList = z.object({
+    title: z.string({
+        required_error: "Le titre est obligatoire.",
+        invalid_type_error: "Le titre est obligatoire."
+    }).min(3, {
+        message: "Le titre est trop court."
+    }),
+    boardId: z.string()
+});
