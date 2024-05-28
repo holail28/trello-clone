59547a21254fb20aefcccb34748e149d21fbc2db
holail14 <kev20duvin8@gmail.com>
2024-05-28 10:56:30 +0200

    add Card copy & delete

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -10,11 +10,13 @@ export const UpdateCard = z.object({
             message: "La description est trop courte."
         }),
     ),
-    title: z.string({
-        required_error: "Le titre est obligatoire.",
-        invalid_type_error: "Le titre est obligatoire."
-    }).min(3, {
-        message: "Le titre est trop court."
-    }),
+    title: z.optional(
+        z.string({
+            required_error: "Le titre est obligatoire.",
+            invalid_type_error: "Le titre est obligatoire."
+        }).min(3, {
+            message: "Le titre est trop court."
+        })
+    ),
     id: z.string()
 });
--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -81,7 +81,7 @@ export const Header = ({
 Header.Skeleton = function HeaderSkeleton() {
     return (
         <div className="flex items-start gap-x-3 mb-6">
-            <Skeleton className="h-6 w-6 mt-1 bg-neutral-200" />
+            <Skeleton className="h-5 w-5 mt-1 bg-neutral-200" />
             <div>
                 <Skeleton className="w-24 h-6 mb-1 bg-neutral-200" />
                 <Skeleton className="w-12 h-4 bg-neutral-200" />
